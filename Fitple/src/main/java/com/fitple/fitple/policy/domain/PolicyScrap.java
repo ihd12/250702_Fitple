47fc06d30fcaa57d1e52eb7eb4f4de0a34ff3e39
binkim00 <k0b0301@naver.com>
2025-07-02 12:28:44 +0900

    2025.07.02 policy Scrap 추가

---
+++ Fitple/src/main/java/com/fitple/fitple/policy/domain/PolicyScrap.java
@@ -0,0 +1,43 @@
+package com.fitple.fitple.policy.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "policy_scrap")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PolicyScrap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // FK는 걸지 않고, 단순 숫자로만 처리
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "policy_id", length = 100, nullable = false)
+    private String policyId;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
