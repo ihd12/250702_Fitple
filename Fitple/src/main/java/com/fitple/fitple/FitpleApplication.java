3cb6c29a1c8512e205b43c35e2ce893d593647bc
binkim00 <k0b0301@naver.com>
2025-07-02 12:07:44 +0900

    프로젝트 최초 생성

---
+++ Fitple/src/main/java/com/fitple/fitple/FitpleApplication.java
@@ -0,0 +1,13 @@
+package com.fitple.fitple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitpleApplication.class, args);
+    }
+
+}
